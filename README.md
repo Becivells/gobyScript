f058658cdcc354abd08c7282a2fbde614ea1c730
becivells <732903873@qq.com>
2021-05-26 15:00:44 +0800

    init

---
+++ README.md
@@ -0,0 +1,155 @@
+# goby EXP 辅助工具
+
+
+
+`goby-cmd` 的图形界面测试太麻烦，命令行测试poc的参数太长，也太麻烦，提交的时候需要按照一定的格式。附带测试的案例。此工具提供了代码格式化(需要安装[go](https://studygolang.com/dl) 并且设置环境变量)，代码检查(需要安装[golangci](https://golangci-lint.run/usage/install/#local-installation)), 还有测试。
+
+## 示例
+
+### 最简模式
+
+目录下只有一个go文件。文件末尾带有目标
+
+```
+├── pocfile.yaml
+├── pocfile.go
+├── README.md
+```
+
+`pocfile.go`
+
+```
+.
+.
+.
+			}
+			return expResult
+		},
+	))
+}
+
+// http://target:port
+```
+
+#### 测试poc
+
+```shell
+gobyexp --poc
+```
+
+#### 测试exp
+
+```shell
+gobyexp --exp
+```
+
+目录下多个文件可以使用 `-t` 参数指定其中一个
+
+### 普通模式
+
+#### 生成 `cve` poc
+
+`goby-cmd`
+
+```
+goby-cmd -mode genpoc -CVEID CVE-2020-15505 -exportFile test.go
+```
+
+`gobyexp`
+
+```
+ gobyexp -g -c CVE_2020-13167 -e Netsweeper_webAdmin_RCE_CVE_2020-13167.go
+```
+
+#### 测试 poc
+
+`goby-cmd`
+
+```shell
+goby-cmd -mode runpoc -target http://target:port -pocFile pocfile.go -operation scan
+```
+
+`gobyexp`
+
+```shell
+gobyexp -u http://target:port -t pocfile.go --poc 
+```
+
+#### 测试 exp
+
+`goby-cmd`
+
+```shell
+goby-cmd -mode runpoc -target http://target:port -pocFile pocfile.go -params {"cmd":"whoami"} -operation exploit
+```
+
+`gobyexp`
+
+```shell
+gobyexp -u http://target:port -t pocfile.go --poc
+```
+
+#### 打印相关命令
+
+```
+gobyexp -u http://target:port -t pocfile.go  --poc --test
+gobyexp -u http://target:port -t pocfile.go  --exp --test
+```
+
+### 格式化代码和检查
+
+```
+gobyexp -u http://target:port -t pocfile.go  --poc --test
+```
+
+如果不需要代码检查和格式化可以使用 `--nolint --nofmt ` 
+
+## 环境变量
+
+如果有环境变量则按照环境变量来
+
+```
+goby_params # 用于exp执行的时候传值
+goby_proxy  # 用于获取cve信息时候的代理地址
+```
+
+## 注意事项
+编写时请加入`//nolint`
+```shell script
+ExpManager.AddExploit(NewExploit( //nolint
+```
+
+
+```shell script
+usage: gobyexp.py [-h] [--nolint] [--nofmt] [--poc | --exp] [--test] [-u URL] [-t TEMPLATE] [-p PARAMS] [-g] [-c CVEID] [-e EXPORTFILE] [--proxy PROXY]
+
+goby-cmd poc fast test you can set env export goby_params='{"cmd":"whoami"}' or export goby_proxy="http://127.0.0.1:8080"
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --nolint              golangci-lint check default true
+  --nofmt               gofmt code
+
+genpoc:
+  genpoc template from cveid
+
+  -g, --genpoc          genpoc
+  -c CVEID, --cveid CVEID
+                        test url
+  -e EXPORTFILE, --exportFile EXPORTFILE
+                        test url
+  --proxy PROXY         proxy with cve search default None
+
+poc/exp:
+  test poc or exp
+
+  --poc                 poc mode
+  --exp                 exp mode
+  --test                only print command and not execute
+  -u URL, --url URL     test target
+  -t TEMPLATE, --template TEMPLATE
+                        goby exp template
+  -p PARAMS, --params PARAMS
+                        default '{"cmd":"whoami"}'
+
+```
---
+++ gobyexp.py
@@ -0,0 +1,169 @@
+#!/usr/local/bin/python3
+
+import os
+import sys
+import glob
+import shutil
+import argparse
+import subprocess
+
+
+def run_CMD(cmdlist):
+    p = subprocess.Popen(cmdlist, shell=False, universal_newlines=True)
+    p.communicate()
+    if p.returncode != 0:
+        sys.exit(p.returncode)
+
+def lint_check(args):
+    if shutil.which("golangci-lint"):
+        print("[+] check template %s style"%args.template)
+
+        lintcmd = ["golangci-lint","run",args.template,
+                   # "--disable-all",
+                   "-D","typecheck",
+                   "--enable",
+                   "asciicheck"
+                   ",dogsled"
+                   ",dupl"
+                   ",gofumpt"
+                   ",goheader"
+                   ",goimports"
+                   ",gofmt"
+                   ",ifshort"
+                   ",whitespace"
+                   ",wsl"
+                   ]
+        print("[+][lint] cmd: %s" % " ".join(lintcmd))
+        run_CMD(lintcmd)
+    else:
+        print("[-] please install golangci-lint we need check")
+
+def gofmt(args):
+    if shutil.which("gofmt"):
+        print("[+] gofmt template %s" % args.template)
+        fmtcmd = ["gofmt", '-s', '-w', args.template]
+        print("[+][gofmt] cmd: %s" % " ".join(fmtcmd))
+        p = subprocess.Popen(fmtcmd, shell=False, universal_newlines=True)
+        p.wait()
+    else:
+        print("[-] please install golang we need gofmt")
+
+
+def gen_POC(args):
+    if args.genpoc:
+        if os.environ.get("goby_proxy"):
+            args.proxy = os.environ.get("goby_proxy")
+        if not args.cveid or not args.exportFile:
+            print("cve or exportFile not none")
+            sys.exit(1)
+
+        cmd = [
+            "goby-cmd",
+            "-mode", "genpoc",
+            "-CVEID", args.cveid,
+            "-exportFile", args.exportFile
+        ]
+        if args.proxy:
+            cmd.append("-proxy")
+            cmd.append(args.proxy)
+        if not args.test:
+            print("[+][genpoc] cmd: %s" % " ".join(cmd))
+            run_CMD(cmd)
+        else:
+            print("[+][genpoc] cmd: %s" % " ".join(cmd))
+        sys.exit(0)
+
+
+def run_POCEXP(args):
+    cmd = [
+        "goby-cmd",
+        "-mode", "runpoc",
+        "-target", args.url,
+        "-pocFile", args.template,
+        "-params", args.params
+    ]
+    mode = "poc"
+    if args.poc:
+        cmd.append("-operation")
+        cmd.append("scan")
+    else:
+        mode = "exp"
+        cmd.append("-operation")
+        cmd.append("exploit")
+
+    if not args.test:
+        print("[+][%s] cmd: %s" % (mode," ".join(cmd)))
+        run_CMD(cmd)
+    else:
+        print("[+][%s] cmd: %s" % (mode," ".join(cmd)))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='goby-cmd poc fast test '
+                                                 'you can set env '
+                                                 'export goby_params=\'{"cmd":"whoami"}\' '
+                                                 'or '
+                                                 'export goby_proxy="http://127.0.0.1:8080"')
+
+    genpoc = parser.add_argument_group("genpoc","genpoc template from cveid")
+    pocexp = parser.add_argument_group("poc/exp","test poc or exp")
+    group=pocexp.add_mutually_exclusive_group()
+    parser.add_argument("--nolint",action='store_true',help="golangci-lint check default true")
+    parser.add_argument("--nofmt", action='store_true', help="gofmt code")
+    group.add_argument("--poc", action='store_true', help="poc mode")
+    group.add_argument("--exp", action='store_true', help="exp mode")
+    pocexp.add_argument("--test", action='store_true', help="only print command and not execute")
+    pocexp.add_argument("-u", "--url", type=str,  help="test target")
+    pocexp.add_argument("-t", "--template", type=str, help="goby exp template")
+    pocexp.add_argument("-p", "--params", default='{"cmd":"whoami"}', help="default '{\"cmd\":\"whoami\"}' ")
+    genpoc.add_argument("-g", "--genpoc", action='store_true', help="genpoc")
+    genpoc.add_argument("-c", "--cveid", type=str, help="test url")
+    genpoc.add_argument("-e", "--exportFile", type=str, help="test url")
+    genpoc.add_argument("--proxy", default="", help="proxy with cve search default  None")
+    args = parser.parse_args()
+
+    gen_POC(args)
+
+    if not args.template:
+        tfile = glob.glob("*.go")
+        if len(tfile) == 1:
+            args.template = tfile[0]
+            print("[+] use templates %s" % (args.template))
+        elif len(tfile) >= 1:
+            print("[-] templates more than one,please use -t/--templates")
+        else:
+            print("[-] not find template please change dir or use: -t/--templates")
+            sys.exit(1)
+    if not os.path.exists(args.template):
+        print(args.template)
+        print("[-] args templates path not find")
+        sys.exit(1)
+
+    if not args.nofmt:
+        gofmt(args)
+
+    if not args.nolint:
+        lint_check(args)
+
+    if not args.url:
+        with open(args.template, 'r') as f:
+            lines = f.read().strip().split("\n")
+            lastline = lines[-1].strip()
+            if lastline.startswith("//"):
+                args.url = lastline.lstrip("/").strip()
+                print("[+] use target %s from template %s" % (args.url,args.template))
+
+    if not args.url:
+        print("the following arguments are required: -u/--url")
+        sys.exit(-1)
+
+    if os.environ.get("goby_params"):
+        args.params =os.environ.get("goby_params")
+
+    if not args.poc and not args.exp:
+        print("[-] the following arguments are required: --poc/--exp ")
+        sys.exit(1)
+
+    run_POCEXP(args)
+
+
+
